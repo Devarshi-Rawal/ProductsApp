f06c67d841a723f945bc87011886235fc21c9c39
Devarshi Rawal <jdsrawal558@gmail.com>
2024-10-13 16:37:13 +0530

    Implemented databinding in MainActivity

---
+++ app/src/main/java/com/example/productsapp/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.productsapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.productsapp.R
+import com.example.productsapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
