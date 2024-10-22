e7f2f0356c561f322e669f577b81ac138c17cfe2
devarshi.rawal <devarshi.rawal@promptsoftech.in>
2024-10-22 18:20:34 +0530

    Login module under development

--- app/src/main/java/com/example/productsapp/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/productsapp/ui/auth/AuthViewModel.kt
@@ -1,19 +1,37 @@
 package com.example.productsapp.ui.auth
 
+import android.text.Editable
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.productsapp.ui.auth.data.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AuthViewModel @Inject constructor(private val repository: AuthRepository) : ViewModel(){
 
-    fun loginUser(email: String, password: String){
+    val emailStateFlow: StateFlow<String> = repository._emailMutableStateFlow
+    val passwordStateFlow: StateFlow<String> = repository._passwordMutableStateFlow
+
+    fun afterEmailTextChanged(editable: Editable?){
         viewModelScope.launch {
-            repository.loginUser(email,password)
+            repository.afterEmailTextChanged(editable)
+        }
+    }
+
+    fun afterPasswordTextChanged(editable: Editable?){
+        viewModelScope.launch {
+            repository.afterPasswordTextChanged(editable)
+        }
+    }
+
+    fun loginUser(){
+        viewModelScope.launch {
+            repository.loginUser(emailStateFlow.toString(),passwordStateFlow.toString())
         }
     }
 }
--- app/src/main/java/com/example/productsapp/ui/auth/data/AuthRepository.kt
+++ app/src/main/java/com/example/productsapp/ui/auth/data/AuthRepository.kt
@@ -2,25 +2,40 @@ package com.example.productsapp.ui.auth.data
 
 import android.app.Application
 import android.content.SharedPreferences
+import android.text.Editable
 import android.util.Log
 import com.example.productsapp.app.ProductsApp
 import com.example.productsapp.networking.AuthApiService
 import com.example.productsapp.ui.auth.login.models.LoginReqBody
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 class AuthRepository @Inject constructor(private val authApiService: AuthApiService) {
 
+    val _emailMutableStateFlow: MutableStateFlow<String> = MutableStateFlow("")
+    val _passwordMutableStateFlow: MutableStateFlow<String> = MutableStateFlow("")
+
+    suspend fun afterEmailTextChanged(editable: Editable?){
+        _emailMutableStateFlow.emit(editable.toString())
+    }
+
+    suspend fun afterPasswordTextChanged(editable: Editable?){
+        _passwordMutableStateFlow.emit(editable.toString())
+    }
+
     suspend fun loginUser(email: String, password: String) {
-        val loginResponse = authApiService.loginUser(LoginReqBody(password,email))
-        if (loginResponse.isSuccessful){
-            if (loginResponse.code() == 200){
-                ProductsApp().setLoginToken(loginResponse.body()?.token.toString())
-                Log.e("LoginUser", "fetchProducts: Login Success")
+        if (email.isNotEmpty() && password.isNotEmpty()){
+            val loginResponse = authApiService.loginUser(LoginReqBody(password,email))
+            if (loginResponse.isSuccessful){
+                if (loginResponse.code() == 200){
+                    ProductsApp().setLoginToken(loginResponse.body()?.token.toString())
+                    Log.e("LoginUser", "fetchProducts: Login Success")
+                } else{
+                    Log.e("LoginUser", "fetchProducts: Login Unsuccessful")
+                }
             } else{
                 Log.e("LoginUser", "fetchProducts: Login Unsuccessful")
             }
-        } else{
-            Log.e("LoginUser", "fetchProducts: Login Unsuccessful")
         }
     }
 }
